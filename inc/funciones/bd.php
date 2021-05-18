b3126f1ea5cfbb937a4714de895a4c90970d2d5b
manuela-cepeda <79425483+manuela-cepeda@users.noreply.github.com>
2021-05-18 12:59:44 +0200

    Update bd.php

--- inc/funciones/bd.php
+++ inc/funciones/bd.php
@@ -1,10 +1,10 @@
 <?php
 //credenciales de la base de datos
 
-define('DB_USUARIO', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
-define('DB_NOMBRE', 'agendaphp');
+define('DB_USUARIO', 'mmeiobkozewfyc');
+define('DB_PASSWORD', '271355a9f6d13799aeabd8f58ebb91ed9c478d648d2c430415a1e2e2072b680d');
+define('DB_HOST', 'ec2-52-209-134-160.eu-west-1.compute.amazonaws.com');
+define('DB_NOMBRE', 'd5qolsmqdf8h50');
 
 $conn = new mysqli(DB_HOST, DB_USUARIO, DB_PASSWORD, DB_NOMBRE ); //opcion de quinto parametro si no funciona: el puerto o port
 
